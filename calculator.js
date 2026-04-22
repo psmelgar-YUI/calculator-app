8d8ca344b5c8cccca85cbc43b34129500c950dbe
melgarprincestephen-gif <melgarprincestephen@gmail.com>
2026-04-22 18:30:48 +0800

    Step 10-11: added calculateDifference function

--- calculator.js
+++ calculator.js
@@ -4,3 +4,10 @@ function calculateSum(num1, num2) {
 console.log(calculateSum(2, 5));
 console.log(calculateSum(10, 10));
 console.log(calculateSum(5, 5));
+
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
+console.log(calculateDifference(22, 5));
+console.log(calculateDifference(12, 1));
+console.log(calculateDifference(17, 9));

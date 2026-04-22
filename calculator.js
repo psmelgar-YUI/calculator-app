7644e239efa74146497726812ce6d8052969a244
melgarprincestephen-gif <melgarprincestephen@gmail.com>
2026-04-22 18:31:35 +0800

    Step 16-17: added calculateSquare function

--- calculator.js
+++ calculator.js
@@ -25,3 +25,9 @@ function calculateQuotient(num1, num2) {
 }
 console.log(calculateQuotient(7, 11));
 console.log(calculateQuotient(3, 0));
+
+function calculateSquare(num) {
+  return num ** 2;
+}
+console.log(calculateSquare(2));
+console.log(calculateSquare(9));
